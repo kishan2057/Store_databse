1828640adcd2162d236ab00aa091e2b4ace531c2
kishan kumar yadav <61868272+kishan2057@users.noreply.github.com>
2026-04-27 19:37:17 +0545

    Remove server port logging

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,4 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error(err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
